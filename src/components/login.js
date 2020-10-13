c242e83018c3fa2ac076cbf71fee217ebf62f286
Christopher Gonzales <cm.gonzales@yahoo.com>
2020-10-12 19:55:35 -0700

    added redux

--- src/components/login.js
+++ src/components/login.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const Login = () => {
+
+    const[input, newInput] = useState('')
+    const [name, newName] = useState('')
 
 
 const handleChange = (e) =>{
     e.preventDefault()
+    newInput(e.target.value)
 }    
 
 const handleSubmit = (e) => {
     e.preventDefault()
     console.log('test')
+
+    newName(input)
 }
 
   return (
@@ -19,12 +26,13 @@ const handleSubmit = (e) => {
           Name:
           <input
             type="text"
-            value
+            value = {input}
             onChange={handleChange}
           />
         </label>
         <input type="submit" value="Submit" />
       </form>
+      <h1>{name}</h1>
     </div>
   );
 }
